2f33fb7b88fd88e338ec2387d66740c4709b3326
simonelle <97528755+simonelle@users.noreply.github.com>
2023-04-26 11:58:38 -0400

    linux notes cronjob

---
+++ hello.sh
@@ -0,0 +1,13 @@
+#!#/bin/bash
+echo "hello everyone"
+echo "welcome to Linux Environmat"
+echo "i love devops"
+echo "today's date is .."
+free -m 
+touch elis.sh 
+date
+pwd 
+whoami
+top 
+uptime
+df -h
